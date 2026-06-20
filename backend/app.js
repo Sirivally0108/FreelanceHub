544015a04d4b4a8565177394aa5481a969c2e75a
192472371 <192472371.simats@saveetha.com>
2026-06-20 15:40:50 +0530

    Added authentication APIs

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
-
 const app = express();
 
-// Middleware
 app.use(express.json());
+
+// Import Routes
+const authRoutes = require('./routes/authRoutes');
+
+// Use Routes
+app.use('/api/auth', authRoutes);
 
 // Test Route
 app.get('/', (req, res) => {
